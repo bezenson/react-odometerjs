eef5052c44e9d548364d1fbacdd19f63ec9b2f31
Vladislav Bezenson <vladvvb@gmail.com>
2016-12-16 14:14:30 +0300

    React Odometer.js component

---
+++ index.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import Odometer from 'odometer';
+
+class ReactOdometer extends React.Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired,
+    options: PropTypes.object,
+  };
+
+  componentDidMount() {
+    this.odometer = new Odometer({
+      el: ReactDOM.findDOMNode(this),
+      value: this.props.value,
+      ...this.props.options,
+    });
+  }
+
+  componentDidUpdate() {
+    this.odometer.update(this.props.value);
+  }
+
+  render() {
+    return React.createElement('div');
+  }
+}
+
+export default ReactOdometer;
